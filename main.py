332ba81f01920db581dd91d211d5b14098d8f49a
enzortorres <enzortorres2005@gmail.com>
2025-03-26 16:55:58 -0300

    adicionei a opcao de baixar audio

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from pytubefix import YouTube
 from pathlib import Path
+import os
 import platform
 
 sistema = platform.system()
 # > Seleciona a pasta (Vídeos) de acordo com o sistema operacional
-if sistema == 'Windows': # ? Windowns
-    pasta_videos = Path.home() / "Videos" 
-elif sistema == 'Darwin': # ? macOS
+if sistema == 'Windows': # ? Windows
+    pasta_videos = Path.home() / "Videos"
+elif sistema == 'Darwin': # ? Mac-OS
     pasta_videos = Path.home() / "Movies"
-else: # ? Linux e outros Unix-like
-    pasta_videos = Path.home() / "Videos" 
+else: # ? Linux e outros
+    pasta_videos = Path.home() / "Videos"
 
-pasta_destino = pasta_videos / "youtube_downloads" # > Cria uma pasta (youtube_downloads) dentro da pasta vídeos
-pasta_destino.mkdir(parents=True, exist_ok=True) # > Confirma se a pasta existe
+pasta_destino = pasta_videos / "youtube_downloads"
+pasta_destino.mkdir(parents=True, exist_ok=True)
 
-url = input("Digite a url do vídeo que deseja baixar: ").strip()
+url = input("Digite a URL do vídeo que deseja baixar: ").strip()
 
 try:
-    yt = YouTube(url) # > Cria o objeto Youtube
-    
-    stream = yt.streams.get_highest_resolution() # > Seleciona o stream com a melhor resolução
-    stream.download(output_path=pasta_destino) # > Baixa o vídeo para o diretório atual
-    
-    print("Download completo!")
+    opcao = int(input("O que deseja baixar?\n1 - Vídeo\n2 - Áudio\n>>> "))
+    yt = YouTube(url)
+
+    print(f"Baixando: {yt.title}")
+
+    if opcao == 1:
+        stream = yt.streams.get_highest_resolution()
+        stream.download(output_path=pasta_destino)
+        print("Download de vídeo completo!")
+
+    elif opcao == 2:
+        stream = yt.streams.filter(only_audio=True).first()
+        arquivo_temp = stream.download(output_path=pasta_destino)
+
+        print("Download de áudio completo!")
+    else:
+        print("Opção inválida.")
+
 except Exception as error:
     print("Download falhou")
     print(f"\033[1;31m{error.__class__.__name__}: {error}\033[0m")
